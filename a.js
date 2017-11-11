27378dd926cf000cf63f81a857ab7ae204a4fc9e
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-11 08:16:48 +0700

    a

---
+++ a.js
@@ -0,0 +1,10 @@
+// const a = new Array()
+const a = ['Node', 'React', 'Android', 'iOS'];
+
+a.push('PHP', 'ABCD');
+// a.unshift('PHP');
+// a.shift();
+// a.pop();
+// a.splice(1, 2);
+// a.splice(1, 0, 'PHP', 'Native');
+console.log(a);
